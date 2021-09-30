1f4fd229663d1679ccdbdc9b3ef19321a2114b7c
jun0222 <nextseason6@gmail.com>
2021-09-30 23:34:06 +0900

    add ~section8

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi! I'm Jun. I excite now Next.js practice!</p>
+        <p>Next.jsの練習です</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
