d1d673810353df6ae6f7cbf0aa0037da8e8955c9
jun0222 <nextseason6@gmail.com>
2021-09-30 23:24:45 +0900

    Revert "add ~section6"
    This reverts commit 04a50c261b3c2aa573cebebc2d4477623030d387.
    
    Revert "add ~section5"
    
    This reverts commit 25a255814bc0836a736fcf6571ed3fe3a05d3b79.

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,8 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
 
-// export async function getStaticProps() {
-  export async function getServerSideProps(context) {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home({ allPostsData }) {
+export default function Home() {
   return (
     <Layout home>
       <Head>
@@ -27,22 +14,6 @@ export default function Home({ allPostsData }) {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
       </section>
     </Layout>
   )
